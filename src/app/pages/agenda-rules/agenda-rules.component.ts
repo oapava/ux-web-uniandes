c8677f2489e3c81e656cb8bfe86047e9e61690af
Omar Pava <omarpava@MacBook-Pro-de-Omar.local>
2025-09-22 22:53:56 -0500

    feat: se ajusta vista de reglas de agenda

--- src/app/pages/agenda-rules/agenda-rules.component.ts
+++ src/app/pages/agenda-rules/agenda-rules.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IconButtonComponent } from '../../shared/icon-button/icon-button.component';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-agenda-rules',
@@ -9,5 +10,20 @@ import { IconButtonComponent } from '../../shared/icon-button/icon-button.compon
   styleUrl: './agenda-rules.component.scss'
 })
 export class AgendaRulesComponent {
+  form: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+          diasSemana: this.fb.group({
+            lun: [true], mar: [false], mie: [false], jue: [false],
+            vie: [false], sab: [false], dom: [false]
+          })
+        });
+  }
+
+  cancelar() {
+    this.form.reset({
+      diasSemana: { lun: true, mar: false, mie: false, jue: false, vie: false, sab: false, dom: false }
+    });
+  }
 
 }
